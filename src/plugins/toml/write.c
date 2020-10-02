da6d5099df76f07793c1bbc17e0f8d23adfddf1e
Jakob Fischer <jakobfischer93@gmail.com>
2020-10-02 16:19:18 +0200

    toml: Added basic metakey preservation.
    Metakeys are now written as comments with a prefix on writing.
    On reading, those special comments will get assigned as metakeys to a
    key.
    
    This is currently just a very basic implementation, which assumes that
    the metakey name/values don't have any special characters / are binary.
    Also, metakey names are currently expected to not contain any spaces.
    
    Tests for this changes not yet added/adapted.

--- src/plugins/toml/write.c
+++ src/plugins/toml/write.c
@@ -21,6 +21,7 @@
 
 #include "error.h"
 #include "integer.h"
+#include "meta.h"
 #include "node.h"
 #include "prepare.h"
 #include "type.h"
@@ -63,6 +64,7 @@ static int writeClosingSequence (Node * node, Writer * writer);
 static int writeTableArrayHeader (const char * name, Writer * writer);
 static int writeScalar (Key * key, Writer * writer);
 static int writeQuoted (const char * value, char quoteChar, int quouteCount, Writer * writer);
+static int writeMetakeys (Key * key, Writer * writer);
 static int writePrecedingComments (const CommentList * commentList, Writer * writer);
 static int writeInlineComment (const CommentList * commentList, bool emitNewline, Writer * writer);
 static int writeComment (const CommentList * comment, Writer * writer);
@@ -196,6 +198,7 @@ static void writerError (Writer * writer, int err, const char * format, ...)
 	}
 }
 
+
 static int writeTree (Node * node, Writer * writer)
 {
 	int result = 0;
@@ -205,6 +208,8 @@ static int writeTree (Node * node, Writer * writer)
 		comments = collectComments (node->key, writer);
 		result |= writePrecedingComments (comments, writer);
 	}
+
+	result |= writeMetakeys (node->key, writer);
 
 	if (node->type == NT_SIMPLE_TABLE)
 	{
@@ -479,6 +484,20 @@ static bool isMultilineString (const char * str)
 	return false;
 }
 
+static int writeMetakeys (Key * key, Writer * writer)
+{
+	int result = 0;
+	keyRewindMeta (key);
+	const Key * meta;
+	while ((meta = keyNextMeta (key)) != NULL)
+	{
+		if (shouldWriteMetakey (keyName (meta)))
+		{
+			result |= writeMetakeyAsComment (meta, writer->f);
+		}
+	}
+	return result;
+}
 
 static int writePrecedingComments (const CommentList * commentList, Writer * writer)
 {
@@ -548,6 +567,9 @@ static int writeFileTrailingComments (Key * parent, Writer * writer)
 		result |= writePrecedingComments (comments, writer);
 		freeComments (comments);
 	}
+
+	result |= writeMetakeys (parent, writer);
+
 	return result;
 }
 
